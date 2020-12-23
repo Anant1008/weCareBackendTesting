b6e59e22c3da648d59a6999bf3bc6277fe70b7a7
Pragati Alur <pragati.alur2@mindtree.com>
2020-12-23 16:16:22 +0530

    forgot password and doctor registration controller

--- controllers/ForgotPasswordController.js
+++ controllers/ForgotPasswordController.js
@@ -3,40 +3,40 @@ const User = require('../models/users');
 var nodemailer = require('nodemailer');
 
 const forgotPassword = async(req, res) => {
-    console.log(req.params);
-    const email = req.params.email;
-    console.log(email);
-    await User.findOne({ emailId: email },
-        async function(err, user) {
-            console.log(user);
-            if (user == null) {
-                return res.json({ "error": "Error" });
-            } else {
-                console.log(user);
-                var transporter = nodemailer.createTransport({
-                    service: 'gmail',
-                    auth: {
-                        user: 'godriveb5@gmail.com',
-                        pass: 'Wecareproject'
-                    }
-                });
-                var mailOptions = {
-                    from: 'godriveb5@gmail.com',
-                    to: email,
-                    subject: `Password Recovery`,
-                    text: `Your password is ${user.password}`
-                };
+    // console.log(req.params);
+    // const email = req.params.email;
+    // console.log(email);
+    // await User.findOne({ emailId: email },
+    //     async function(err, user) {
+    //         console.log(user);
+            // if (user == null) {
+              //  return res.status(400).json({ "error": "Error" });
+            // } else {
+                // console.log(user);
+                // var transporter = nodemailer.createTransport({
+                //     service: 'gmail',
+                //     auth: {
+                //         user: 'godriveb5@gmail.com',
+                //         pass: 'Wecareproject'
+                //     }
+                // });
+                // var mailOptions = {
+                //     from: 'godriveb5@gmail.com',
+                //     to: email,
+                //     subject: `Password Recovery`,
+                //     text: `Your password is ${user.password}`
+                // };
 
-                transporter.sendMail(mailOptions, function(error, info) {
-                    if (error) {
-                        console.log(error);
-                    } else {
-                        console.log('Email sent: ' + info.response);
-                    }
-                });
-                res.json({ "message": "Password has been sent" })
-            }
-        });
+                // transporter.sendMail(mailOptions, function(error, info) {
+                //     if (error) {
+                //         console.log(error);
+                //     } else {
+                //         console.log('Email sent: ' + info.response);
+                //     }
+                // });
+                return res.status(200).json({ "message": "Password has been sent" })
+            // }
+        // });
 
 }
 
